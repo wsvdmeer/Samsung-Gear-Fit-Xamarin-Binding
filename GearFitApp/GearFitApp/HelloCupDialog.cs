397f6a8bb9a396967e5d3055198b89275f7cd6fc
Wiebe Steven van der Meer <wiebesteven@freed.nl>
2014-11-24 22:03:48 +0100

    First Commit with the CupSdk Binding project and the GearFitApp sample project

---
+++ GearFitApp/GearFitApp/HelloCupDialog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Android.Content;
+using Android.Widget;
+using Com.Samsung.Android.Sdk.Cup;
+//http://forum.xda-developers.com/gear-fit/development/samsung-gear-fit-sdk-available-t2872803/page2
+namespace GearFitApp
+{
+  public class HelloCupDialog:ScupDialog
+  {
+    Context context;
+   
+    public HelloCupDialog(Context context):base(context){
+      this.context = context;
+    }
+     
+
+    protected override void OnCreate(){
+      base.OnCreate ();
+      BackEnabled = true;
+      ScupLabel helloLabel = new ScupLabel (this);
+      helloLabel.Text = "Hello Android";
+      helloLabel.Alignment=  ScupLabel.AlignCenter;
+      helloLabel.Width = ScupLabel.FillDialog;
+      helloLabel.Height = ScupLabel.FillDialog;
+      helloLabel.Show ();
+      this.BackPressed += onBackPressed;
+    }
+      
+    void onBackPressed (object sender, BackPressedEventArgs e)
+    {
+      this.Finish ();
+    }
+  }
+
+ 
+}
---
+++ GearFitApp/GearFitApp/MainActivity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Com.Samsung.Android.Sdk.Cup;
+
+namespace GearFitApp
+{
+  [Activity (Label = "GearFitApp", MainLauncher = true, Icon = "@drawable/icon")]
+  public class MainActivity : Activity
+  {
+   
+    private HelloCupDialog mHelloCupDialog = null;
+
+
+
+    protected override void OnCreate (Bundle bundle) 
+    {
+      base.OnCreate (bundle);
+
+     
+      // Set our view from the "main" layout resource
+      SetContentView (Resource.Layout.Main);
+     
+      Scup scup = new Scup();
+      try {
+        Console.WriteLine ("init scup");
+        scup.Initialize(BaseContext);
+
+      }catch (Exception e){
+        Console.WriteLine ("no scup : " + e.Message);
+      }
+
+      var title = FindViewById<TextView> (Resource.Id.title);
+      title.Click += onClick;
+
+      //mHelloCupDialog = new HelloCupDialog(this);
+    }
+
+    void onClick (object sender, EventArgs e)
+    {
+      Console.WriteLine ("click");
+      mHelloCupDialog = new HelloCupDialog(this);
+    }
+
+  }
+}
+
+
